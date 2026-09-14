6f205d782f15f8ddde3c67ccd3696850da642b7c
Andy Edwards <jedwards@fastmail.com>
2026-09-14 16:07:51 -0500

    feat(@jcoreio/toolchain-monorepo-test-foo): initial commit

---
+++ packages/foo/release.config.cjs
@@ -0,0 +1,10 @@
+/* eslint-env node, es2018 */
+const base = require('@jcoreio/toolchain-semantic-release/release.config.cjs')
+module.exports = {
+  ...base,
+  plugins: base.plugins.filter((p) =>
+    /semantic-release\/(commit-analyzer|release-notes-generator)/.test(
+      Array.isArray(p) ? p[0] : p
+    )
+  ),
+}
